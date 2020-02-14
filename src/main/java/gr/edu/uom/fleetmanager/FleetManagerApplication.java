d8c0a5315bedc74067506d68763ef3f1ec551123
alexc <alexchih02@gmail.com>
2020-02-14 21:11:26 +0200

    initial commit

---
+++ src/main/java/gr/edu/uom/fleetmanager/FleetManagerApplication.java
@@ -0,0 +1,13 @@
+package gr.edu.uom.fleetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/edu/uom/fleetmanager/FleetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.edu.uom.fleetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
